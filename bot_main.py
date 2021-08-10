67004a5d6d12642ece7416212406877f32ac5e69
Mikhail Afanasev <a.mike@bk.ru>
2021-08-10 22:22:00 +0300

    Initial commit

---
+++ bot_main.py
@@ -0,0 +1,17 @@
+# https://github.com/netology-code/py-advanced-diplom/tree/new_diplom
+
+from vk_bot import VkBot
+from vk_api.longpoll import VkEventType
+
+bot = VkBot()
+
+for event in bot.talk.longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            if not bot.user.is_defined(event.user_id):
+                bot.set_user(event.user_id)
+            bot.write_reply(event.text)
+
+
+
+
